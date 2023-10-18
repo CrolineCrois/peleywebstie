d97967151fe089754a81b454da6be85f4c967a16
CrolineCrois <maoninii@gmail.com>
2023-10-18 12:21:40 -0700

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link"
+
 export default function NavBar(){
     return (
-        <section className="py-1">
-
+        <section className="px-1.5 py-1 bg-slate-600/50 flex justify-between">
+            <Link href="/">
+                home image
+            </Link>
+            <section>
+                <NavLinks title="Capstone" path=""/>
+            </section>
         </section>
+    )
+}
+
+function NavLinks(props: {title: string, path: string}){
+    return (
+        <div>
+            <Link href={props.path}>
+                <h1>
+                    {props.title}
+                </h1>
+            </Link>
+        </div>
     )
 }
