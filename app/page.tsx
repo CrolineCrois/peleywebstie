86d625449ebed2f7e69c032579ce1380a6d3b79b
penguin212 <dhuber06@gmail.com>
2023-10-20 12:00:13 -0700

    fixed color

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <body className="bg-[#101010] w-full h-screen">
+      <body className="bg-[#181818] w-full h-screen">
         <NavBar/>
             <h1 className="flex w-full h-1/2 text-white text-9xl font-montserrat justify-center align-bottom items-end">
                 Paleyontology
