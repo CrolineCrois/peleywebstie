36dfbd65739c9de06413db0f5a09de95c0da7b25
penguin212 <dhuber06@gmail.com>
2023-10-20 11:01:06 -0700

    added background and title

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main>
       <NavBar/>
+      <body className="bg-[#202020] w-full h-full">
+        <h1 className="flex w-full text-white text-6xl font-montserrat">
+          Paleyontology
+        </h1>
+      </body>
     </main>
-  )
-}
+    
+  );
+};
