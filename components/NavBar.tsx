66385547117dc2b7e4dc13c10dfcf5836fc6b2e5
CrolineCrois <maoninii@gmail.com>
2023-10-20 11:40:29 -0700

    navbar spacing and styleing

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,16 @@ import Link from "next/link"
 
 export default function NavBar(){
     return (
-        <section className="px-1.5 py-1 bg-slate-600/50 flex justify-between text-white">
-            <Link href="/">
-                home image
+        <section className="px-4 py-6 bg-slate-600/0 flex justify-center items-center text-white relative">
+            <Link href="/" className="absolute left-2">
+                <img src="lambda.png" className="h-14 ">
+                </img>
             </Link>
-            <section>
+            <section className="flex justify-evenly w-full">
                 <NavLinks title="Capstone" path=""/>
+                <NavLinks title="APCSA" path=""/>
+                <NavLinks title="FOOP" path=""/>
+                <NavLinks title="APCSP" path=""/>
             </section>
         </section>
     )
@@ -17,7 +21,7 @@ function NavLinks(props: {title: string, path: string}){
     return (
         <div>
             <Link href={props.path}>
-                <h1>
+                <h1 className="text-2xl">
                     {props.title}
                 </h1>
             </Link>
